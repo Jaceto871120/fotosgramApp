644f6ca7426a7f9596e5911dd8c31308bd025c29
Jair Celis <jair.celis.torrado@gmail.com>
2020-07-08 17:52:38 -0500

    Se agrega componente para selecciona Avatar

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -14,45 +14,6 @@ export class LoginPage implements OnInit {
 
   @ViewChild('slidePrincipal', {static: true}) slides: IonSlides;
 
-  avatars = [
-    {
-      img: 'av-1.png',
-      seleccionado: true
-    },
-    {
-      img: 'av-2.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-3.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-4.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-5.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-6.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-7.png',
-      seleccionado: false
-    },
-    {
-      img: 'av-8.png',
-      seleccionado: false
-    },
-  ];
-
-  avatarSlide = {
-    slidesPerView: 3.5
-  };
-
   loginUser = {
     email: 'pauline.barreto@gmail.com',
     password: '12345678'
@@ -67,7 +28,7 @@ export class LoginPage implements OnInit {
 
   constructor(private usuarioService: UsuarioService,
               private navController: NavController,
-              private uiService : UiServiceService) { }
+              private uiService: UiServiceService) { }
 
   ngOnInit() {
     this.slides.lockSwipes(true);
@@ -101,11 +62,6 @@ export class LoginPage implements OnInit {
       // Mostrar alerta de usuario y contrasena
       this.uiService.alertaInformativa('Ese correo electronico ya existe');
     }
-  }
-
-  seleccionarAvatar(avatar) {
-    this.avatars.forEach(av => av.seleccionado = false);
-    avatar.seleccionado = true;
   }
 
   mostrarRegistro() {
